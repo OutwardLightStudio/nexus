e2360a8f88d50635ecea96a51f88fb82d2981251
Brylie Christopher Oxley <brylie@protonmail.com>
2024-12-14 20:38:23 +0200

    Add desired_rotation handling

--- scenes/moving_hazard.gd
+++ scenes/moving_hazard.gd
@@ -1,27 +1,41 @@
 extends AnimatableBody3D
 
-@export var destination: Vector3
-@export var duration: float = 1
+## Target position relative to the object's initial position
+@export var desired_position: Vector3 = Vector3.ZERO
 
-# Called when the node enters the scene tree for the first time.
+## Target rotation in degrees relative to the initial rotation
+@export var desired_rotation: Vector3 = Vector3.ZERO
+
+## Time in seconds for one complete animation cycle
+@export var desired_duration: float = 1.0
+
+var _initial_transform: Transform3D
+
 func _ready() -> void:
-	var tween = create_tween()
-	tween.set_loops()
+	_initial_transform = transform
+	
+	# Create target transform from position and rotation
+	var target_transform = Transform3D()
+	target_transform = target_transform.translated(desired_position)
+	target_transform = target_transform.rotated(Vector3.RIGHT, deg_to_rad(desired_rotation.x))
+	target_transform = target_transform.rotated(Vector3.UP, deg_to_rad(desired_rotation.y))
+	target_transform = target_transform.rotated(Vector3.FORWARD, deg_to_rad(desired_rotation.z))
+	
+	var tween = create_tween().set_loops()
 	tween.set_trans(Tween.TRANS_SINE)
+	
+	# Forward phase: Tween to combined transform
 	tween.tween_property(
 		self,
-		"global_position",
-		global_position + destination,
-		duration,
+		"transform",
+		_initial_transform * target_transform,
+		desired_duration
 	)
-	tween.tween_property(
+	
+	# Return phase: Tween back to initial transform
+	tween.chain().tween_property(
 		self,
-		"global_position",
-		global_position,
-		duration,
+		"transform",
+		_initial_transform,
+		desired_duration
 	)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
